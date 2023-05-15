f1e4a4a852d58f0d07ca2ae7f230886f57f95531
vivekp2412 <vivekp2412@gmail.com>
2023-05-15 12:09:33 +0530

    ENV File Config in Staging Branch
    ENV and Package Config Modified
    
    App file error solved
    
    Final Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import React from "react";
 import "./App.css";
 import Container from "./components/Container";
-
+const env = import.meta.env;
 function App() {
   return (
     <>
+      <p className="mode-name">In {env.VITE_ENV} Enviroment</p>
       <Container />
     </>
   );
